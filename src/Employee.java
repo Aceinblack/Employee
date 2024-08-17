72ec012c57f767033e84c95c69955f02b3b324b5
Brinsly <ybrinsly@gmail.com>
2024-08-17 06:12:00 -0500

    First commit

---
+++ src/Employee.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Employee {
+	protected String firstName;
+	protected String lastName;
+	protected int employeeID;
+	protected double salary;
+	
+	public Employee() {
+		firstName = "";
+		lastName = "";
+		employeeID = 0;
+		salary = 0.0;
+	}
+	
+	public Employee(String firstName, String lastName, int employeeID, int salary) {
+		this.setFirstName(firstName);
+		this.setLastName(lastName);
+		this.setEmployeeID(employeeID);
+		this.salary = salary;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+	
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public int getEmployeeID() {
+		return employeeID;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+	
+	public void employeeSummary() {
+		System.out.println("Employee Name: " + getFirstName() + " " + getLastName());
+		System.out.println("Employee ID: " + getEmployeeID());
+		System.out.println("Salary: " + getSalary());
+	}
+	
+}
---
+++ src/TestEmployee.java
@@ -0,0 +1,15 @@
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		Manager employee1 = new Manager();
+		
+		employee1.setFirstName("James");
+		employee1.setLastName("Kluger");
+		employee1.setEmployeeID(9999);
+		employee1.setSalary(340000);
+		employee1.setDepartment("Sales");
+
+	}
+
+}

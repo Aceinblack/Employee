36c4f61639220013b6f24ba70385090e35c3a88c
Brinsly <ybrinsly@gmail.com>
2024-08-17 06:17:00 -0500

    Added new Employee object and initialized it

--- src/TestEmployee.java
+++ src/TestEmployee.java
@@ -3,13 +3,17 @@ public class TestEmployee {
 
 	public static void main(String[] args) {
 		Manager employee1 = new Manager();
+		Employee employee2;
 		
 		employee1.setFirstName("James");
 		employee1.setLastName("Kluger");
 		employee1.setEmployeeID(9999);
 		employee1.setSalary(340000);
 		employee1.setDepartment("Sales");
-
+		
+		employee2 = new Employee("Janet", "Joestar", 10034, 65000);
+		
+		
 	}
 
 }
